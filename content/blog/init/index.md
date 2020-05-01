b3aa3093e8a352dd52fb76895d0ed782d8d5537e
anton-maslakov <antoxam1997@gmail.com>
2020-05-01 11:56:23 +0300

    Link open new tab

---
+++ content/blog/init/index.md
@@ -0,0 +1,10 @@
+---
+title: Init
+date: "2020-05-06"
+description: "my first post"
+---
+
+
+> Hello there! It's me:
+
+![me](./me.jpg)
---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+// custom typefaces
+import "typeface-montserrat"
+import "typeface-merriweather"
+
+// require("src/syles/global.css")
+
+import "prismjs/themes/prism.css"
